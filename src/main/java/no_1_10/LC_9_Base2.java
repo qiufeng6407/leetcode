fcd24a648ef517197cb140063d56d02da8ee964d
jiangchao <jiangchao@jieyisoft.com>
2020-07-24 18:33:42 +0800

    Leetcode 9: 回文数

---
+++ src/main/java/no_1_10/LC_9_Base2.java
@@ -0,0 +1,28 @@
+package no_1_10;
+
+public class LC_9_Base2 {
+
+    public boolean isPalindrome(int x) {
+    	if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+    	if (x < 10) return true;
+    	int r = 0, t = x;
+    	while (t > 0) {
+    		r = r * 10 + t % 10;
+    		t = t / 10;
+    	}
+    	return r == x;
+    }
+
+	public static void main(String[] args) {
+		LC_9_Base2 lc = new LC_9_Base2();
+
+		assert lc.isPalindrome(-123) == false : "";
+		assert lc.isPalindrome(-121) == false : "";
+		assert lc.isPalindrome(121) == true : "";
+		assert lc.isPalindrome(0) == true : "";
+		assert lc.isPalindrome(12) == false : "";
+		assert lc.isPalindrome(11) == true : "";
+		
+	}
+
+}
