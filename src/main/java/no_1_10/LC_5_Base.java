c98cc079a850f280db9b3082f651c020f3efd427
jiangchao <jiangchao@jieyisoft.com>
2020-07-24 11:43:46 +0800

    Leetcode 5 最大回文

---
+++ src/main/java/no_1_10/LC_5_Base.java
@@ -0,0 +1,47 @@
+package no_1_10;
+public class LC_5_Base {
+
+	public String longestPalindrome(String s) {
+		if (s == null || s.isEmpty()) {
+			return "";
+		}
+		
+		int start = 0, end = 0;
+		for (int i = 0; i < s.length(); i ++) {
+			int x = i, y = i + 1;
+			while (x >= 0 && y < s.length() && s.charAt(x) == s.charAt(y)) {
+				x --;
+				y ++;
+			}
+			if (y - x> end -start) {
+				start = x; end = y;
+			}
+			
+			x = i - 1; y = i + 1;
+			while (x >= 0 && y < s.length() && s.charAt(x) == s.charAt(y)) {
+				x --;
+				y ++;
+			}
+			if (y - x > end -start) {
+				start = x; end = y;
+			}
+		}
+		
+		return s.substring(start + 1, end);
+    }
+
+	public static void main(String[] args) {
+		LC_5_Base lc = new LC_5_Base();
+		
+		System.out.println(lc.longestPalindrome("abcabcbb")); // bcb
+		System.out.println(lc.longestPalindrome("bbbbb")); // bbbbb
+		System.out.println(lc.longestPalindrome("pwwkew")); // ww
+		System.out.println(lc.longestPalindrome("abcdaaaaaaedgtsa")); // aaaaaa
+		System.out.println(lc.longestPalindrome("au")); // a
+		System.out.println(lc.longestPalindrome("a")); // a
+		System.out.println(lc.longestPalindrome("aab")); // aa
+		System.out.println(lc.longestPalindrome("babad")); // aba
+		System.out.println(lc.longestPalindrome("cbbd")); // bb
+	}
+
+}
