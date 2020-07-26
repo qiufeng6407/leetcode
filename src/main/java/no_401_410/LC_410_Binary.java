5298dfdd128b5361f056c342632030292db22950
jiangchao <jiangchao@jieyisoft.com>
2020-07-26 17:44:44 +0800

    Leetcode 329: 矩阵中的最长递增路径

--- src/main/java/no_401_410/LC_410_Binary.java
+++ src/main/java/no_401_410/LC_410_Binary.java
@@ -1,47 +1,53 @@
 package no_401_410;
 
 public class LC_410_Binary {
+	public int splitArray(int[] nums, int m) {
+		return splitArray(nums, m, 0);
+	}
 
-		public int splitArray(int[] nums, int m) {
-			long max = 0, min = 0;
-			for (int i = 0; i < nums.length; i++) {
-				min = Math.max(min, nums[i]);
-				max += nums[i];
-			}
-			if (nums.length == m) return (int) min;
-			if (m == 1) return (int) max;
-			if (max == 0) return 0;
-			
-			while (min < max) {
-				// 使用二分查找这里的判断要是min<max
-				// 比如[0, 2, 0, 3, 10, 15, 0, 3, 6], m=3
+	public int max(int[] nums, int from) {
+		return max(nums, from, nums.length);
+	}
 
-				// 当mid = 27, 查找到 [0, 2, 0, 3, 10], [15, 0, 3, 6], m=2不满足条件
-				// 当mid = 21, 查找到 [0, 2, 0, 3, 10], [15, 0, 3], [6]
-				// 这个时候子数组总和最大值为18, 虽然也能满足m=3, 但是18实际并不是最小值
-				// 所以需要将max设置为21, 并继续使用二分查找
-				
-				long mid = (max + min) >>> 1;
-				long sumt = 0;
-				int mt = 1;
-				for (int i = 0; i < nums.length; i++) {
-					if (sumt + nums[i] > mid) {
-						sumt = nums[i];
-						mt ++;
-					} else {
-						sumt += nums[i];
-					}
-				}
-				System.out.println(min + " " + max + " " + mid + ", M=" + mt);
-				if (mt > m) min = mid + 1; else max = mid;
-			}
-			if (min > Integer.MAX_VALUE || min < Integer.MIN_VALUE) return 0; else return (int) min;
+	public int max(int[] nums, int from, int to) {
+		int max = -1;
+		for (int i = from; i < to; i++) {
+			if (nums[i] > max)
+				max = nums[i];
 		}
+		return max;
+	}
+
+	public int sumfrom(int[] nums, int from) {
+		return sumfrom(nums, from, nums.length);
+	}
+
+	public int sumfrom(int[] nums, int from, int to) {
+		int sum = 0;
+		for (int i = from; i < to; i++) {
+			sum += nums[i];
+		}
+		return sum;
+	}
+
+	public int splitArray(int[] nums, int m, int from) {
+		if (m == 1) return sumfrom(nums, from);
+		if (nums.length - from == m) return max(nums, from);
+		int min = -1;
+		for (int i = from + 1; i - from + m <= nums.length; i++) {
+			int sum = sumfrom(nums, from, i);
+			if (min >= 0 && sum >= min) return min;
+			int child = splitArray(nums, m - 1, i);
+			int thismin = Math.max(sum, child);
+			if (min < 0 || thismin < min) min = thismin;
+		}
+		return min;
+	}
 
 	public static void main(String[] args) {
 		LC_410_Binary lc = new LC_410_Binary();
-		System.out.println("||" + lc.splitArray(new int[] { 1, Integer.MAX_VALUE }, 1));
-		System.out.println("||" + lc.splitArray(new int[] { 1, Integer.MAX_VALUE }, 2));
+
+		System.out.println("||" + lc.splitArray(new int[] { 2, 3, 1, 2, 4, 3 }, 5));
 		System.out.println("||" + lc.splitArray(new int[] { 7, 2, 5, 10, 8 }, 2));
 		System.out.println("||" + lc.splitArray(new int[] { 0, 2, 0, 3, 10, 15, 0, 3, 6 }, 3));
 
