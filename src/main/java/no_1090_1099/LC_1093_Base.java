c0260493ed76870dfde8948057d2a43b52682cd0
jiangchao <jiangchao@jieyisoft.com>
2020-08-17 15:13:09 +0800

    1093. 大样本统计

---
+++ src/main/java/no_1090_1099/LC_1093_Base.java
@@ -0,0 +1,56 @@
+package no_1090_1099;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ * @author jlgc
+ *
+ */
+public class LC_1093_Base {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	class Solution {
+
+		public boolean isBalanced(TreeNode root) {
+			Map<TreeNode, Integer> depMap = new HashMap<>();
+			return isBalanced(root, depMap);
+		}
+
+		public boolean isBalanced(TreeNode root, Map<TreeNode, Integer> depMap) {
+			if (root == null) {
+				return true;
+			}
+			return (Math.abs(depth(root.left, depMap) - depth(root.right, depMap)) <= 1) && isBalanced(root.left)
+					&& isBalanced(root.right);
+		}
+
+		private int depth(TreeNode root, Map<TreeNode, Integer> depMap) {
+			if (root == null) {
+				return 0;
+			}
+			if (depMap.containsKey(root)) {
+				return depMap.get(root);
+			}
+			int leftDep = depth(root.left, depMap);
+			if (root.left != null) {
+				depMap.put(root.left, leftDep);
+			}
+			int rightDep = depth(root.right, depMap);
+			if (root.left != null) {
+				depMap.put(root.right, rightDep);
+			}
+			return Math.max(leftDep, rightDep) + 1;
+		}
+	}
+
+}
